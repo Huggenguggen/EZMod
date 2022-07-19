bee2e729b2d0a4ed6a561231ad190c48530312d5
Yu Heng Lee <lee2000yh@gmail.com>
2022-07-19 15:48:36 +0800

    Added time period at the top right

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import { Link } from 'react-router-dom';
@@ -8,12 +8,31 @@ import { IconContext } from 'react-icons';
 
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
+  const [currSem, setCurrSem] = useState("");
 
   const showSidebar = () => setSidebar(!sidebar);
+
+  useEffect(() => {
+    var finalRes = ""
+    var today = new Date(),
+    date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    if (today.getMonth() + 1 <= 4) {
+      finalRes = (today.getFullYear() - 1) + "-" + (today.getFullYear()) + " SEM 2";
+    } else if (today.getMonth() + 1 <= 7) {
+      finalRes = (today.getFullYear()) + " SUMMER BREAK";
+    } else if (today.getMonth() + 1 <= 11) {
+      finalRes = (today.getFullYear()) + "-" + (today.getFullYear() + 1) + " SEM 1";
+    } else {
+      finalRes = today.getFullYear() + " WINTER BREAK"
+    }
+    
+    setCurrSem(finalRes);
+  }, [])
 
   return (
     <>
       <IconContext.Provider value={{ color: '#fff' }}>
+        <div id='topline'>
         <div className={sidebar ? "navbar-sidebar" : "navbar"}>
           <Link to='#' className='menu-bars'>
             <FaIcons.FaBars onClick={showSidebar} />
@@ -21,6 +40,12 @@ function Navbar() {
           <h1 id='appname'>
             EZMOD
           </h1>
+        </div>
+        <div id='semester'>
+          <h1>
+            {currSem}
+          </h1>
+        </div>
         </div>
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
           <ul className='nav-menu-items' onClick={showSidebar}>
